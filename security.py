4479a3549d91de6a34ac08eea78ac11f6dc4ef48
KulDim <vadim.culya.2017@gmail.com>
2021-10-21 19:13:25 +0300

    Refactoring

--- security.py
+++ security.py
@@ -7,19 +7,15 @@ from modulus.FPS import FPS; FPS = FPS(); SEVE_FPS = int()
 
 video_capture = cv2.VideoCapture(0)
 
-
-
 # Settings
-INT_DELAY_TIME = 30
+DELAY = INT_DELAY_TIME = 30
+IS_OPEN = bool
 progressBar = 0
 peopleSearch = True
-delay = INT_DELAY_TIME
-_is_open = bool
 
-
+font = cv2.FONT_HERSHEY_DUPLEX
 known_face_encodings = []
 known_face_names = []
-
 
 def getPictures(directory):
     check = False
@@ -31,7 +27,6 @@ def getPictures(directory):
         known_face_names.append(name)
         check = True
     return check
-
 
 openFileImg = getPictures('face/')
 
@@ -51,74 +46,49 @@ while openFileImg:
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
-
-
-
-
-
-
-
-
+            # Свой не свой 
             if True in matches:
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+                progressBar = (progressBar + 10)
             else:
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            if True in matches:
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
-            else:
                 cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                progressBar = (progressBar - 10)
 
-            font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-            if True in matches:
-                progressBar = progressBar + 10
-
-            else:
-                progressBar = progressBar - 10
-
-            if progressBar >= 110:
+            if progressBar >= 100 + 10: 
                 progressBar = 0
                 peopleSearch = False
-                _is_open = True
-
-            elif progressBar <= -60:
+                IS_OPEN = True
+            elif progressBar <= -50 - 10:
                 progressBar = 0
                 peopleSearch = False
-                _is_open = False
+                IS_OPEN = False
 
+    # DELAY OPEN OR NOT OPEN
     if not peopleSearch:
-        delay = delay - 1
-        font = cv2.FONT_HERSHEY_DUPLEX
+        DELAY = (DELAY - 1)
 
-        if _is_open:
-            cv2.putText(frame, 'OPEN THE DOOR', (10, 30), font, 1.0, (0, 255, 0), 5)
-        if not _is_open:
-            cv2.putText(frame, 'GO AWAY', (10, 30), font, 1.0, (0, 0, 255), 5)
+        if IS_OPEN: cv2.putText(frame, 'OPEN THE DOOR', (10, 30), font, 1.0, (0, 255, 0), 5)
+        else: cv2.putText(frame, 'GO AWAY', (10, 30), font, 1.0, (0, 0, 255), 5)
 
-        if(delay <= 0):
-            delay = INT_DELAY_TIME
+        if(DELAY <= 0):
+            DELAY = INT_DELAY_TIME
             peopleSearch = True
-        else:
-            print('DELAY: ' + str(delay))
-
-    print('progressBar: ' + str(progressBar))
-    font = cv2.FONT_HERSHEY_DUPLEX
+        else: cv2.putText(frame, 'DELAY: ' + str(DELAY), (10, 140), font, 1.0, (0, 0, 255), 5)
 
     # Frames per second
     counter = FPS.frameСounter()
     if counter != None: SEVE_FPS = counter
-
     cv2.putText(frame, 'FPS: ' + str(int(SEVE_FPS)), (10, 65), font, 1.0, (0, 0, 255), 5)
-
+    # progressBar
     cv2.putText(frame, 'progressBar :' + str(progressBar) + '%', (10, 100), font, 1.0, (0, 0, 255), 5)
-
+    
     cv2.imshow('Video', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
+    if cv2.waitKey(25) & 0xFF == ord('q'): break
 
 video_capture.release()
 cv2.destroyAllWindows()
-
-
