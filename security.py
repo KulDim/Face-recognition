38bc6de9222e2dc20954265af3499405b90c67e6
KulDim <vadim.culya.2017@gmail.com>
2021-10-21 10:45:05 +0300

    progressBar

--- security.py
+++ security.py
@@ -8,6 +8,7 @@ video_capture = cv2.VideoCapture(0)
 known_face_encodings = []
 known_face_names = []
 
+progressBar = 0
 
 is_file_img = False
 directory = 'face/'
@@ -21,10 +22,10 @@ for img in files:
 
 while is_file_img:
     ret, frame = video_capture.read()
+
     rgb_frame = frame[:, :, ::-1]
     face_locations = face_recognition.face_locations(rgb_frame)
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
         name = "Unknown"
@@ -52,8 +53,21 @@ while is_file_img:
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         if True in matches:
-            print('open the door')
+            progressBar = progressBar + 10
+        else:
+            progressBar = progressBar - 10
 
+
+        if progressBar >= 100:
+            progressBar = 0
+
+            print('OPEN THE progressBar')
+        elif progressBar <= 0:
+            progressBar = 0
+
+
+        print('progressBar: ' + str(progressBar))
+        
     cv2.imshow('Video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
